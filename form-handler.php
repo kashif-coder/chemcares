38ae8d5d6586fef7762c359142a28b3f2b722d20
kashif-coder <kashifalirps@gmail.com>
2023-12-27 20:44:11 +0530

    First commit

---
+++ form-handler.php
@@ -0,0 +1,27 @@
+<?
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+$email_from = 'info@chemcares.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "user Name: $name.\n".
+               "user Email: $visitor_email.\n".
+               "Subject: $subject.\n". 
+               "user Message: $message.\n".
+
+$to = 'kamagcontracting@gmail.com';
+
+$headers= "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+?>
